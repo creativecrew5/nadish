4764264092ddbe80f13da263613c9c3852d62ba0
Saud Alam <saualam@cisco.com>
2016-09-08 15:26:09 +0530

    Updated Config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,7 @@
 export class AppConfig { 
-   config = {
-    ID:'366660555051-2j1psaps2nph96e2jvpr50q4j9vmo3bg.apps.googleusercontent.com',
-    SECRET:'i5WVk8aKkc-tRxhcUOuUQ_co'
-  };
+  config = {};
   constructor() {
-  
-  	
-
+ 
   } 
   get(prop?: any) { 
     return this.config.hasOwnProperty(prop) ? this.config[prop] : null;
